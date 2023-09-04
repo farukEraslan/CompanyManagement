dd75c7667240fc01faff1a258df3a965b78f19e7
Faruk Eraslan <faruk.eraslan@bilgeadam.com>
2023-09-04 11:10:53 +0300

    .

--- CompanyManagement.WebUI/GlobalUsing.cs
+++
@@ -1,8 +0,0 @@
-﻿global using CompanyManagement.DataAccess.Context;
-global using CompanyManagement.Entities.Concrete;
-global using Microsoft.EntityFrameworkCore;
-global using AutoMapper;
-global using CompanyManagement.Core.Utilities.Helpers;
-global using CompanyManagement.Dtos.Account;
-global using Microsoft.AspNetCore.Identity;
-global using Microsoft.AspNetCore.Mvc;
