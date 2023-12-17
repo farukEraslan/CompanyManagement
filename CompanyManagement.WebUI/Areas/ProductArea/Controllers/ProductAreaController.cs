5cd799dd874790e06fd9446f163ceec1990e8ec5
farukEraslan <frk.eraslan@hotmail.com>
2023-12-18 00:47:03 +0300

    Product table edited.

---
+++ CompanyManagement.WebUI/Areas/ProductArea/Controllers/ProductAreaController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyManagement.WebUI.Areas.ProductArea.Controllers
+{
+    [Area("product")]
+    public class ProductAreaController : Controller
+    {
+        [HttpGet("{area}/{controller}/{action}")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("{area}/{controller}/{action}")]
+        public IActionResult Create() 
+        {
+            return View();
+        }
+    }
+}
